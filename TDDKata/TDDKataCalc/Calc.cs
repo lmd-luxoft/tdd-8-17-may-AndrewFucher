ea13ea4f78bcf1f68f8fc15357f1201c2554a9ff
wind-bora <matches707@gmail.com>
2023-05-10 11:16:27 +0300

    Implement Add method

--- TDDKata/TDDKataCalc/Calc.cs
+++ TDDKata/TDDKataCalc/Calc.cs
@@ -4,24 +4,23 @@ public class Calc
 {
     public int Add(string digits)
     {
-        throw new NotImplementedException();
-        // if (string.IsNullOrWhiteSpace(digits))
-        //     return 0;
-        //
-        // var stringDigits = digits.Split(",");
-        //
-        // var sum = 0;
-        // foreach (var stringDigit in stringDigits)
-        // {
-        //     if (int.TryParse(stringDigit, out var digit))
-        //     {
-        //         sum += digit;
-        //         continue;
-        //     }
-        //
-        //     return -1;
-        // }
-        //
-        // return sum;
+        if (string.IsNullOrWhiteSpace(digits))
+            return 0;
+        
+        var stringDigits = digits.Split(",");
+        
+        var sum = 0;
+        foreach (var stringDigit in stringDigits)
+        {
+            if (int.TryParse(stringDigit, out var digit))
+            {
+                sum += digit;
+                continue;
+            }
+        
+            return -1;
+        }
+        
+        return sum;
     }
 }
